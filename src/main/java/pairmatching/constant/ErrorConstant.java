5d27ba0ad4cea451914e22be6fbe99a379749deb
wonseok <tkddls23@konkuk.ac.kr>
2022-12-16 18:24:35 +0900

    feat(constant): 상수 생성

---
+++ src/main/java/pairmatching/constant/ErrorConstant.java
@@ -0,0 +1,9 @@
+package pairmatching.constant;
+
+public class ErrorConstant {
+
+    public static final String ERROR_NO_MATCHING_HISTORY = "[ERROR] 매칭 이력이 없습니다.";
+    public static final String ERROR_NOT_VALID_INPUT = "[ERROR] 올바른 값을 입력해주세요.";
+    public static final String ERROR_CREW_ALREADY_MEET = "[ERROR] 만난적 있는 크루끼리 페어가 됐습니다. 재매칭을 시도합니다.";
+
+}
