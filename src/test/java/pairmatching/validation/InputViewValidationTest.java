f2ac9cb5fb7948903bfa6d7b348ab037c1571909
wonseok <tkddls23@konkuk.ac.kr>
2022-12-16 18:44:55 +0900

    test(validation: InputViewValidation): 재매칭 여부 값 검증 테스트

--- src/test/java/pairmatching/validation/InputViewValidationTest.java
+++ src/test/java/pairmatching/validation/InputViewValidationTest.java
@@ -9,12 +9,21 @@ import static pairmatching.constant.ErrorConstant.ERROR_NOT_VALID_INPUT;
 
 class InputViewValidationTest {
 
-    @DisplayName("예외 처리 : 입력값이 올바르지 않을때 에러 발생 검증")
+    @DisplayName("예외 처리 : 기능입력값이 올바르지 않을때 에러 발생 검증")
     @ParameterizedTest
     @ValueSource(strings = {"10h","h","4", "", " "})
-    void checkNotValidDigitInputValidation(String input) {
+    void checkInputValidation(String input) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> InputViewValidation.checkOptionValid(input))
+                .withMessageStartingWith(ERROR_NOT_VALID_INPUT);
+    }
+
+    @DisplayName("예외 처리 : 재매칭 여부 입력값이 올바르지 않을때 에러 발생 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"10h","네니오","", " "})
+    void checkReMatchCommandValidation(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputViewValidation.checkReMatchCommandValid(input))
                 .withMessageStartingWith(ERROR_NOT_VALID_INPUT);
     }
 }
